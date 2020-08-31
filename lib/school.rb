92c6a9a7a7c337f54c1962da908d5d76f523a681
Sierra Wallace <sskwallace@gmail.com>
2020-08-31 01:35:02 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,12 +8,12 @@ class School
     @roster
   end
   def add_student(name,grade)
-    @roster[grade] = []
-    @roster[grade] << name
-  end
-  def grade(grade)
-    @roster[grade]
-  end
+    @name = name 
+    @grade = grade
+    if @roster.include?(grade) == false 
+      @roster[grade] = [] 
+    end
+    @roster[grade] << name 
 end
-
+end
 
